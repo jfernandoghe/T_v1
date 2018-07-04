6fa1e6d25aa03f52860dc1021ad1e6a2f63e422f
jfernandoghe <jfernandoghe@gmail.com>
2018-07-04 00:07:55 -0500

    4-Julio

--- master_comp.m
+++ master_comp.m
@@ -32,7 +32,8 @@ npe=pung-2;
     Vsal2
     [v1]=EditDistance(Vsal1,Vsal2)
     lev(Vsal1,Vsal2)
-    [Score, Alignment] = nwalign(Vsal1,Vsal2)
+%     [Score, Alignment] = nwalign(Vsal1,Vsal2)
+%     [Score, Alignment] = ScanMatch_nwAlgo(Vsal1,Vsal2)
 end
 
 function [vsal,str1, str2] = edit_dist (rawx, rawy, inte)
@@ -49,10 +50,17 @@ if size(rawx,2)==size(rawy,2)
                 for k=1:size(by,2)
                     if rawy(i)<by(k)
                         str2=horzcat(str2,valy(k));
-%                         vsal(band)=(i*j);
-                        vsal(band)=char(33+(i*j));
-                        band=band+1;
-                        break;
+                        %                         vsal(band)=(i*j);
+                        if ((i*j)<=25)
+                            vsal(band)=char(65+(i*j));
+                            band=band+1;
+                            break;
+                        else if (((i*j)>=25)||((i*j)<=50))
+                            vsal(band)=char(96+(i*j));
+                            band=band+1;
+                            break;
+                            end
+                        end
                     end
                 end
                 break;
@@ -70,17 +78,17 @@ end
 % end
 % for o=0:size(by,2)
 %     for p=size(bx,2)
-%         
+%
 %     end
 % end
 end
 
 function [V,v] = EditDistance(string1,string2)
-% Edit Distance is a standard Dynamic Programming problem. Given two 
-% strings s1 and s2, the edit distance between s1 and s2 is the minimum 
-% number of operations required to convert string s1 to s2. The following 
+% Edit Distance is a standard Dynamic Programming problem. Given two
+% strings s1 and s2, the edit distance between s1 and s2 is the minimum
+% number of operations required to convert string s1 to s2. The following
 % operations are typically used:
-% 
+%
 % Replacing one character of string by another character.
 % Deleting a character from string
 % Adding a character to string
@@ -93,7 +101,7 @@ function [V,v] = EditDistance(string1,string2)
 % replace(t,d) , replace(c,p) , replace(l,o) , delete(e)
 % using the other output, you can see the matrix solution to this problem
 %
-% by : 
+% by :
 % Reza Ahmadzadeh (seyedreza_ahmadzadeh@yahoo.com - reza.ahmadzadeh@iit.it)
 % 14-11-2012
 
@@ -124,26 +132,125 @@ function d = lev(s,t)
 % or substitutions required to transform s to t.
 % https://en.wikipedia.org/wiki/Levenshtein_distance
 
-    s = char(s);
-    t = char(t);
-    m = length(s);
-    n = length(t);
-    x = 0:n;
-    y = zeros(1,n+1);   
-    for i = 1:m
-        y(1) = i;
-        for j = 1:n
-            c = (s(i) ~= t(j)); % c = 0 if chars match, 1 if not.
-            y(j+1) = min([y(j) + 1
-                          x(j+1) + 1
-                          x(j) + c]);
-        end
-        % swap
-        [x,y] = deal(y,x);
+s = char(s);
+t = char(t);
+m = length(s);
+n = length(t);
+x = 0:n;
+y = zeros(1,n+1);
+for i = 1:m
+    y(1) = i;
+    for j = 1:n
+        c = (s(i) ~= t(j)); % c = 0 if chars match, 1 if not.
+        y(j+1) = min([y(j) + 1
+            x(j+1) + 1
+            x(j) + c]);
     end
-    d = x(n+1);
+    % swap
+    [x,y] = deal(y,x);
+end
+d = x(n+1);
 end
 
+function [score path step F] = ScanMatch_nwAlgo(intseq1,m,intseq2,n,ScoringMatrix,gap)
+%SCANMATCH_NWALGO Standard Needleman-Wunsch algorithm implementation
+% This is originally from the Matlab Bioinformatics toolbox but was partially 
+% changed to work with ScanMatch  
+
+%   Part of the ScanMatch toolbox
+%   From the BioInformatics Toolbox, Modified by Filipe Cristino 
+%   $Version: 1.00 $  $Date: 10/09/2009
+
+% set up storage for dynamic programming matrix
+F = zeros(n+1,m+1);
+F(2:end,1) = gap * (1:n)';
+F(1,2:end) = gap * (1:m);
+
+% and for the back tracing matrix
+pointer= repmat(uint8(4),n+1,m+1);
+pointer(:,1) = 2;  % up
+pointer(1,1) = 1;  
+
+
+% initialize buffers to the first column
+ptr = pointer(:,2); % ptr(1) is always 4
+currentFColumn = F(:,1);
+
+% main loop runs through the matrix looking for maximal scores
+for outer = 2:m+1
+
+    % score current column
+    scoredMatchColumn = ScoringMatrix(intseq2,intseq1(outer-1));
+    % grab the data from the matrices and initialize some values
+    lastFColumn    = currentFColumn;
+    currentFColumn = F(:,outer);
+    best = currentFColumn(1);
+    
+    for inner = 2:n+1
+        % score the three options
+        up       = best + gap;
+        left     = lastFColumn(inner) + gap;
+        diagonal = lastFColumn(inner-1) + scoredMatchColumn(inner-1);
+
+        % max could be used here but it is quicker to use if statements
+        if up > left
+            best = up;
+            pos = 2;
+        else
+            best = left;
+            pos = 4;
+        end
+
+        if diagonal >= best
+            best = diagonal;
+            ptr(inner) = 1;
+        else
+            ptr(inner) = pos;
+        end
+        currentFColumn(inner) = best;
+
+    end % inner
+    % put back updated columns
+    F(:,outer)   = currentFColumn;
+    % save columns of pointers
+    pointer(:,outer)  = ptr;
+end % outer
+
+% Find the best route throught the scoring matrix
+i = n+1; j = m+1;
+path = zeros(n+m,2);
+step = 1;
+
+while (i > 1 || j > 1)
+    
+    switch pointer(i,j)
+        case 1 % diagonal only
+            j = j - 1;
+            i = i - 1;
+            path(step,:) = [j,i];
+        case 2 % up only
+            i = i - 1;
+            path(step,2) = i;
+        case 4 % left only
+            j = j - 1;
+            path(step,1) = j;
+        case 6 % up or left --> up (favors gaps in seq2)
+            j = j - 1;
+            path(step,1) = j;
+        otherwise %3 diagonal or up   --> diagonal (favors no gaps)
+            %4 diagonal or left       --> diagonal (favors no gaps)
+            %7 diagonal or left or up --> diagonal (favors no gaps)
+            j = j - 1;
+            i = i - 1;
+            path(step,:) = [j,i];
+    end
+    step = step +1;
+end
+
+score =  max(F(n+1,m+1,:));
+
+end
+    
 function [Sal1, Sal2, outl] = RandomWalk2 (ex, ey, n, ne, npe, maxFrechet)
 % % % % % 1920x1080 Screen Resolution
 % % % % % First random point within a square on the middle on the screen
